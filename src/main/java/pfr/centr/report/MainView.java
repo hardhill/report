6c1194f899a4a4cb7f5ecb4c93848caeccfb15af
hardhill <hardhill@mail.ru>
2019-07-04 11:55:58 +0800

    начало разработки MainView

--- src/main/java/pfr/centr/report/MainView.java
+++ src/main/java/pfr/centr/report/MainView.java
@@ -3,14 +3,15 @@ package pfr.centr.report;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Anchor;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Route;
@@ -19,57 +20,68 @@ import pfr.centr.report.models.UserInfo;
 import pfr.centr.report.models.UserLogined;
 
 @Route("")
-public class MainView extends Div implements BeforeEnterObserver, RouterLayout {
+@StyleSheet("styles/styles.css") // Relative to Servlet URL
+public class MainView extends VerticalLayout implements BeforeEnterObserver, RouterLayout {
 
 
     public MainView() {
-        add(BuildTopMenu());
+
+        //CONTAINER
+        setSizeFull();
+        setMargin(false);
+        setSpacing(false);
+        setPadding(false);
+        add(BuildHeader());
         add(ReportMenu());
+        add(BuildWorkspace());
     }
 
     private Component ReportMenu() {
         HorizontalLayout menuBlock = new HorizontalLayout();
-        menuBlock.setMargin(false);
-
         Button bNewReport = new Button();
-        bNewReport.getStyle().set("pading","0px 20px");
         bNewReport.setIcon(new Icon(VaadinIcon.PLUS));
         bNewReport.setText("Новый отчет");
         Button bEditReport = new Button();
         bEditReport.setIcon(new Icon(VaadinIcon.EDIT));
         bEditReport.setText("Редактировать");
-        menuBlock.setWidthFull();
+        menuBlock.addClassName("reportmenu");
         menuBlock.add(bNewReport,bEditReport);
         return menuBlock;
     }
 
 
-    private Component BuildTopMenu(){
+    private Component BuildHeader(){
+        //button 'close'
         NativeButton bClose = new NativeButton();
         bClose.add(new Icon(VaadinIcon.CLOSE));
         bClose.getStyle().set("cursor","pointer");
         bClose.addClickListener(event->{
             UI.getCurrent().navigate(LoginView.class);
         });
+        //logoSide with menu
         HorizontalLayout logoSide = new HorizontalLayout();
         Label logoTitle = new Label("REPORTmaster");
-        logoTitle.setWidth("100px");
-        logoTitle.getStyle().set("margin","0px 25px");
+        logoTitle.addClassName("logoside");
+        Anchor link1 = new Anchor("", "Отчеты");
+        Anchor link2 = new Anchor("price", "Прайс");
+        logoSide.add(logoTitle,link1,link2);
 
-        Anchor link = new Anchor("", "Отчеты");
-
-        logoSide.add(logoTitle,link);
-        HorizontalLayout menuBlock = new HorizontalLayout();
-        menuBlock.setSpacing(false);
-        menuBlock.setPadding(false);
-        menuBlock.setMargin(false);
-        menuBlock.setWidthFull();
-        menuBlock.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
-        menuBlock.setAlignItems(FlexComponent.Alignment.CENTER);
-        menuBlock.getStyle().set("border-bottom","1px solid #d1dfd6");
-        menuBlock.add(logoSide,bClose);
-        return menuBlock;
+        HorizontalLayout reportHeader = new HorizontalLayout();
+        reportHeader.setWidth("100%");
+        reportHeader.setAlignItems(FlexComponent.Alignment.CENTER);
+        reportHeader.getStyle().set("border-bottom","1px solid #d1dfd6");
+        reportHeader.expand(logoSide);
+        reportHeader.add(logoSide,bClose);
+        return reportHeader;
     }
+
+    private Component BuildWorkspace(){
+        VerticalLayout workspace = new VerticalLayout();
+        workspace.addClassName("workspace");
+        workspace.setSizeFull();
+        return workspace;
+    }
+
     private boolean validEnter(){
         boolean otvet = false;
         UserLogined userLogined = (UserLogined) UI.getCurrent().getSession().getAttribute("user");
